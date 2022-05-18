fd0ef351e8914a6cf46d37fe2d2ea2383b2bbb1f
Anthony Assayah <92322613+AnthonyAssayah@users.noreply.github.com>
2022-05-18 15:23:47 +0300

    bash grade 100

--- Test.cpp
+++ Test.cpp
@@ -8,11 +8,12 @@
 using namespace std;
 using namespace ariel;
 
-TEST_CASE("Construct a new Chart"){
+
+TEST_CASE("Check all traversals order on normal case"){
 
     /*
                    A
-                 / | \
+                 / | \    
                 /  |   \
               B    C     D
              / \        /  \
@@ -58,57 +59,43 @@ TEST_CASE("Construct a new Chart"){
 
 
     */
-  
-
-    for (auto it = alphabet.begin_level_order(); it != alphabet.end_level_order(); ++it)
-   {
-    cout << (*it) << endl;
-   }  
+   
  
-    vector<string> vector1 = {"A","B","E","G","H","F","C","D","O","R","S","T","P"};
+    vector<string> vector_lev = {"A","B","C","D","E","F","O","P","G","R","S","T","H"};
     auto iter1 = alphabet.begin_level_order();
-    unsigned int i = 0;
+    uint i = 0;
     while (iter1 != alphabet.end_level_order())
     {
-        CHECK((*iter1) == vector1[i]);
+        CHECK((*iter1) == vector_lev[i]);
         i++;
         ++iter1;
     }
 
-    
+    vector<string> vector_rev = {"H","G","R","S","T","E","F","O","P","B","C","D","A"};
+    auto iter2 = alphabet.begin_reverse_order();
+    uint j = 0;
+    while (iter2 != alphabet.end_reverse_order())
+    {
+        CHECK((*iter2) == vector_rev[j]);
+        j++;
+        ++iter2;
+    }
+
+    vector<string> vector_pre = {"A","B","E","G","H","F","C","D","O","R","S","T","P"};
+    auto iter3 = alphabet.begin_preorder();
+    uint z = 0;
+    while (iter3 != alphabet.end_preorder())
+    {
+        CHECK((*iter3) == vector_pre[z]);
+        z++;
+        ++iter3;
+    }
+
 
 }
 
-TEST_CASE("simple iterators") {
 
-    SUBCASE("==") {
-
-        OrgChart School;
-        School.add_root("Director").add_sub("Director", "Secretary");
-        auto it1 = School.begin();
-        auto it2 = School.begin_level_order();
-        auto it3 = School.begin_reverse_order();
-        auto it4 = School.begin_preorder();
-        CHECK(it1 == it3);
-        CHECK(it1 == it2);
-        CHECK(it2 == it4);
-
-    }
-
-    SUBCASE("++") {
-
-        OrgChart School;
-        School.add_root("Director").add_sub("Director", "Secretary").add_sub("Director", "Supervisor");
-        auto it1 = School.begin();
-        auto it2 = School.begin_level_order();
-        auto it3 = School.begin_reverse_order();
-        auto it4 = School.begin_preorder();
-
-       
-    }
-}
-
-TEST_CASE("Check all traversals order")
+TEST_CASE("Check all traversals order on extrem case")
 {
 
     /*
@@ -148,44 +135,38 @@ TEST_CASE("Check all traversals order")
 
    
 
-
-    vector<string> temp_vector1 = {"ten", "nine", "eight", "seven", "six", "five", "four", "three", "two", "one"};
+    // Check for reverse order traversal
+    vector<string> vector_rev = {"ten", "nine", "eight", "seven", "six", "five", "four", "three", "two", "one"};
     auto it_rev = numbers.begin_reverse_order();
-    unsigned int i = 0;
+    uint i = 0;
     while (it_rev != numbers.end_reverse_order())
     {
-        CHECK((*it_rev) == temp_vector1[i]);
+        CHECK((*it_rev) == vector_rev[i]);
         i++;
         ++it_rev;
     }
 
-    vector<string> temp_vector2 = {"one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten"};
+    // Check for level order traversal
+    vector<string> vector_lev = {"one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten"};
     auto it_lev = numbers.begin_level_order();
     i = 0;
     while (it_lev != numbers.end_level_order())
     {
-        CHECK((*it_lev) == temp_vector2[i]);
+        CHECK((*it_lev) == vector_lev[i]);
         i++;
         it_lev++;
     }
 
-    i = 0;
-
-    for (string element : numbers)
-    {
-        CHECK(element == temp_vector2[i]);
-        i++;
-    }
-
-    auto itPost = numbers.begin_preorder();
+    // Check for preorder traversal
+     auto itPost = numbers.begin_preorder();
     i = 0;
     while (itPost != numbers.end_preorder())
     {
-        CHECK((*itPost) == temp_vector2[i]);
+        CHECK((*itPost) == vector_lev[i]);
         i++;
         itPost++;
     }
-
+   
    cout << numbers << endl;
 
     /*
@@ -201,4 +182,45 @@ TEST_CASE("Check all traversals order")
                                             +- ten
 
     */
+}
+
+
+TEST_CASE("simple iterators") {
+
+    SUBCASE("== / !=") {
+
+        OrgChart School;
+        School.add_root("Director").add_sub("Director", "Secretary");
+        auto it1 = School.begin();
+        auto it2 = School.begin_level_order();
+        auto it3 = School.begin_reverse_order();
+        auto it4 = School.begin_preorder();
+
+        CHECK(it1 == it2);
+        CHECK(it2 == it4);
+        CHECK(it3 != it1);
+        CHECK(it4 != it3);
+
+    }
+
+    SUBCASE("++") {
+
+        OrgChart School;
+        School.add_root("Director").add_sub("Director", "Secretary").add_sub("Director", "Supervisor");
+        auto it1 = School.begin();
+        auto it2 = School.begin_level_order();
+        auto it3 = School.begin_reverse_order();
+        auto it4 = School.begin_preorder();
+
+        CHECK_EQ(it1, it4);
+        CHECK_EQ(it1, it2);
+        CHECK_NOTHROW(it1++);
+        CHECK_NE(it1, it4);
+        CHECK_NOTHROW(it4++);
+        CHECK_EQ(it1, it4);
+        CHECK_NOTHROW(it3++);
+        CHECK_NE(it3, it2);
+
+
+    }
 }
